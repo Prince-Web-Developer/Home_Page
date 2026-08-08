451f40c88987b871fe16f041fcc90fac2ce6868f
Prince-Web-Developer <268293963+Prince-Web-Developer@users.noreply.github.com>
2026-08-08 16:07:42 +0500

    feat: add site header and add theme button
    - users can toggle theme by theme button in header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 import css from "./style.css";
+
+const lightBulb = document.querySelector("#lightBulb");
+const themeButton = document.querySelector("#theme");
+
+
+const isLightMode = window.matchMedia("(prefers-color-scheme: light)");
+changeTheme(isLightMode.matches)
+
+
+
+isLightMode.addEventListener("change", event => {
+    changeTheme(event.matches)
+})
+
+
+themeButton.addEventListener("click", () => {
+    const isWhitetheme = lightBulb.classList.contains("off")
+    changeTheme(!isWhitetheme)
+})
+
+
+function changeTheme(theme) {
+    lightBulb.classList.toggle("off", theme)
+    document.body.classList.toggle("lightTheme", theme)
+    themeButton.setAttribute("aria-pressed",!theme);
+    themeButton.setAttribute(
+      "aria-description",
+      theme ? "light theme is on" : "dark theme is on",
+    );
+}
